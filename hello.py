c4ea927671759c7dee9505c57c48d7ea967ce2a9
davybureau <davy.bureau@gmail.com>
2017-11-03 22:35:41 +0100

    Till server start up

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello World, %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
